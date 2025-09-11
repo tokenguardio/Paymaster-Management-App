51f2c3cbb4aad40ceac5b4812e63489d5c1ca99d
Piotr Józefowicz <pn.jozefowicz@gmail.com>
2025-09-11 21:20:56 +0200

    fix tests errors

--- apps/backend/src/policy/policy.controller.spec.ts
+++ apps/backend/src/policy/policy.controller.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PolicyController } from './policy.controller';
+import { PolicyService } from './policy.service';
 
 describe('PolicyController', () => {
   let controller: PolicyController;
 
+  const mockPolicyService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PolicyController],
+      providers: [
+        {
+          provide: PolicyService,
+          useValue: mockPolicyService,
+        },
+      ],
     }).compile();
 
     controller = module.get<PolicyController>(PolicyController);
--- apps/backend/src/policy/policy.service.spec.ts
+++ apps/backend/src/policy/policy.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '@repo/prisma';
 import { PolicyService } from './policy.service';
 
 describe('PolicyService', () => {
   let service: PolicyService;
 
+  const mockPrismaService = {
+    policy: {
+      create: jest.fn(),
+      findMany: jest.fn(),
+      findUnique: jest.fn(),
+      update: jest.fn(),
+    },
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PolicyService],
+      providers: [
+        PolicyService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
     }).compile();
 
     service = module.get<PolicyService>(PolicyService);
